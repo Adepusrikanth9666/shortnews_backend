77ef9f539e713b78545e0be474283dba02251646
Adepusrikanth9666 <srikanthadepu.17@gmail.com>
2021-01-19 13:49:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors =  require('cors');
+const bodyParser = require('body-parser');
+const mongoose =  require('mongoose');
+require('dotenv').config();
+
+
+const  app = express();
+const port = 7000 ;
+
+console.log(process.env.MONGODB_CONNECTION_STRING);
+
+const whitelist = ['http://localhost:3000']
+const corsOption = {
+  origin:function(origin,callback){
+    if(whitelist.indexOf(origin)!==-1){
+      callback(null,true)
+    }else{
+      callback(new Error('Not allowed by CROS'));
+    }
+  }
+}
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+  console.log(process.env.MONGODB_CONNECTION_STRING);
+  
+  app.listen(port,()=>console.log(`the server has started on port :${port}`));
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+    
+},(err)=>{
+
+    if(err){ throw err }; 
+    console.log("MongoDb connection is established");
+})
+
+
+app.use("/users",require('./routes/user'))
+
+
+
+
